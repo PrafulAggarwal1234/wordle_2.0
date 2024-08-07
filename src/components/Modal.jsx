21d1f2dfeea63aa604390e33d62f235a6032f3bc
Praful <prafulaggarwal1234@gmail.com>
2024-08-07 13:50:30 +0530

    form stylled

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,21 +10,21 @@ export default function Modal({won,turn,solution,setModalFlag}){
                 <div>
                     <h1>You Win!</h1>
                     <p className="solution">{solution}</p>
-                    <p>You found the solution in {turn} guesses :(</p>
+                    <p>You found the solution in {turn} guesses :)</p>
                 </div>
             )}
             {won==-1 && (
                 <div>
                     <h1>Game Over!</h1>
                     {/* <p className="solution">word: {solution}</p> */}
-                    <p>Opponent has guessed the correct answer :)</p>
+                    <p>Opponent has guessed the correct answer :(</p>
                 </div>
             )}
             {won==0 && (
                 <div>
                     <h1>Game Over!</h1>
                     <p className="solution">word: {solution}</p>
-                    <p>Better Luck next time :)</p>
+                    <p>Better Luck next time :(</p>
                 </div>
             )}
             <button onClick={togglePopup}>Close</button>
