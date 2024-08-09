a4295c840744625603685428d4c87c5c4420d6cd
Praful <prafulaggarwal1234@gmail.com>
2024-08-09 16:06:08 +0530

    google-analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-YVV6BFNETZ");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Custom Title" });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
